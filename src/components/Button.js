7633cedaf56d6b7a4d11bee70c0fbf1bfcc1ac40
Joshikanan <118112050+joshikannan@users.noreply.github.com>
2023-05-06 22:21:41 +0530

    test

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+
+const Button = (props) => {
+  return (
+    <button className={props.className}>
+      {props.text} <FiArrowRight />
+    </button>
+  );
+};
+
+export default Button;
